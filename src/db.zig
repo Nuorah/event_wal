bca357e9cfa403c541c37ca8e612612605a0190a
Haroun Moatassime <haroun.moatassime@gmail.com>
2026-02-13 20:53:09 +0100

    Building the wal and the storage is the responsibility of the db library

--- src/db.zig
+++ src/db.zig
@@ -1,12 +1,22 @@
-const Wal = @import("wal.zig");
+const Wal = @import("wal.zig").Wal;
 const std = @import("std");
 
 pub fn Database(comptime EventData: type, comptime StoragesType: type, comptime wal_version: u8) type {
     return struct {
         wal: Wal(EventData, wal_version),
 
-        pub fn loadAllEvents(self: *@This(), allocator: std.mem.Allocator, storages: *StoragesType) !void {
-            const events = try self.wal.readAllBinary(allocator);
+        pub fn init(arena: std.mem.Allocator, wal_path: []const u8) !@This() {
+            return .{
+                .wal = try Wal(EventData, wal_version).init(wal_path, arena),
+            };
+        }
+
+        pub fn deinit(self: *@This()) void {
+            self.wal.deinit();
+        }
+
+        pub fn loadAllEvents(self: *@This(), allocator: std.mem.Allocator, arena: std.mem.Allocator, storages: StoragesType) !void {
+            const events = try self.wal.readAllBinary(arena);
             for (events.items) |evt| {
                 switch (evt.data) {
                     inline else => |payload| try payload.apply(allocator, storages),
@@ -14,9 +24,9 @@ pub fn Database(comptime EventData: type, comptime StoragesType: type, comptime 
             }
         }
 
-        pub fn appendEvent(self: *@This(), allocator: std.mem.Allocator, data: EventData, storages: *StoragesType) !void {
+        pub fn appendEvent(self: *@This(), allocator: std.mem.Allocator, arena: std.mem.Allocator, data: EventData, storages: StoragesType) !void {
             // TODO Add validation
-            try self.wal.appendBinary(allocator, data);
+            try self.wal.appendBinary(arena, data);
             switch (data) {
                 inline else => |payload| try payload.apply(allocator, storages),
             }
--- src/root.zig
+++ src/root.zig
@@ -1,3 +1,4 @@
 pub const Wal = @import("wal.zig").Wal;
 pub const Storage = @import("storage.zig").Storage;
 pub const Event = @import("event.zig").Event;
+pub const Database = @import("db.zig").Database;
